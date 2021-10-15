5bf48d317d32dce0ece48f067efe8c4ab0138e54
Humphryshikunzi <humphryshikunzi9@gmail.com>
2021-10-15 12:19:15 +0300

    removed marketing in Cafrilearn

---
+++ AfriLearnBackend/Migrations/20211015075223_RMMarketing.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AfriLearnBackend.Migrations
+{
+    public partial class RMMarketing : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsMarkettingNotificationsOn",
+                table: "Setting");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsMarkettingNotificationsOn",
+                table: "Setting",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
--- AfriLearnBackend/Models/Setting.cs
+++ AfriLearnBackend/Models/Setting.cs
@@ -2,8 +2,7 @@
 {
     public class Setting : BaseModel
     {
-        public  bool IsAppNotificationsOn { get; set; }
-        public  bool IsMarkettingNotificationsOn { get; set; }
+        public  bool IsAppNotificationsOn { get; set; } 
         public  bool IsNightModeOn { get; set; }
     }
 }
